3714fc0fd2e98d7b794dc606dd09847006ff220d
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-04-09 09:54:16 +0200

    add caps lock for one word

--- keyboards/splitkb/kyria/rev3/keymaps/first/keymap.c
+++ keyboards/splitkb/kyria/rev3/keymaps/first/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSPC,
      CTL_ESC , CH_A ,  CH_R   ,  CH_S  ,   CH_T ,   KC_G ,                                        KC_M,   CH_N ,  CH_E ,   CH_I ,  CH_O , CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , ADJUST, QK_BOOT,     QK_BOOT, KC_RBRC, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                 _______, LT(NAV, KC_TAB), LT(SYM, KC_SPC), LT(NUM, KC_ESC), _______, _______, LT(NUM, KC_ENT),LT(SYM, KC_BSPC), KC_DEL, _______
+                                 _______, LT(NAV, KC_TAB), LT(SYM, KC_SPC), LT(NUM, KC_ESC), QK_CAPS_WORD_TOGGLE, _______, LT(NUM, KC_ENT),LT(SYM, KC_BSPC), KC_DEL, _______
     ),
 
 /*
--- keyboards/splitkb/kyria/rev3/keymaps/first/rules.mk
+++ keyboards/splitkb/kyria/rev3/keymaps/first/rules.mk
@@ -1,2 +1,6 @@
+USER_NAME := crunchyflakes
+
 OLED_ENABLE = yes
 CONVERT_TO = liatris
+
+CAPS_WORD_ENABLE = yes
